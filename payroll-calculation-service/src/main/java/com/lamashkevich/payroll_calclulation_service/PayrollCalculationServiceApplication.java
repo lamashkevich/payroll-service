e68e2cd8ff5c734d78b6051d35472652d48c1b92
Yauheni Lamashkevich <lamashkevich.evg@gmail.com>
2025-04-17 15:43:29 +0300

    Implemented tax-client

--- payroll-calculation-service/src/main/java/com/lamashkevich/payroll_calclulation_service/PayrollCalculationServiceApplication.java
+++ payroll-calculation-service/src/main/java/com/lamashkevich/payroll_calclulation_service/PayrollCalculationServiceApplication.java
@@ -2,8 +2,10 @@ package com.lamashkevich.payroll_calclulation_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class PayrollCalculationServiceApplication {
 
     public static void main(String[] args) {
